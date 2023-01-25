90d8c2fa04582dbad4f208bf6916c6862f4dc9d0
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-01-25 17:20:30 +0100

    updated creazione_db.sql with auto_increment, on delete and on update clauses

--- src/db/creazione_db.sql
+++ src/db/creazione_db.sql
@@ -12,7 +12,7 @@ use db_progetto_tech_web;
 -- _____________ 
 
 CREATE TABLE db_progetto_tech_web.POST (
-     Post_id int not null,
+     Post_id int not null auto_increment,
      Immagine char(100) not null,
      Testo char(100) not null,
      Giorno char(10) not null,
@@ -22,14 +22,14 @@ CREATE TABLE db_progetto_tech_web.POST (
      constraint ID_POST_ID primary key (Post_id));
 
 CREATE TABLE db_progetto_tech_web.Commento (
+     Comment_id int not null auto_increment,
      User_id int not null,
      Post_id int not null,
-     Comment_id int not null,
      Testo char(100) not null,
-     constraint ID_Commento_ID primary key (User_id, Post_id, Comment_id));
+     constraint ID_Commento_ID primary key (Comment_id));
 
 CREATE TABLE db_progetto_tech_web.TAG (
-     Tag_id int not null,
+     Tag_id int not null auto_increment,
      Nome_gioco char(30) not null,
      constraint ID_TAG_ID primary key (Tag_id));
 
@@ -44,7 +44,7 @@ CREATE TABLE db_progetto_tech_web.Segue (
      constraint ID_Segue_ID primary key (Seguito_User_id, Seguace_User_id));
 
 CREATE TABLE db_progetto_tech_web.USER (
-     User_id int not null,
+     User_id int not null auto_increment,
      Username char(30) not null,
      E_mail char(30) not null,
      Password char(30) not null,
@@ -57,35 +57,51 @@ CREATE TABLE db_progetto_tech_web.USER (
 
 alter table POST add constraint FKTaggato_FK
      foreign key (Tag_id)
-     references TAG(Tag_id);
+     references TAG(Tag_id)
+     on update cascade
+     on delete cascade;
 
 alter table POST add constraint FKPostato_FK
      foreign key (User_id)
-     references USER(User_id);
+     references USER(User_id)
+     on update cascade
+     on delete cascade;
 
 alter table Commento add constraint FKRiferito_FK
      foreign key (Post_id)
-     references POST(Post_id);
+     references POST(Post_id)
+     on update cascade
+     on delete cascade;
 
 alter table Commento add constraint FKCreato
      foreign key (User_id)
-     references USER(User_id);
+     references USER(User_id)
+     on update cascade
+     on delete cascade;
 
 alter table Mi_piace add constraint FKMi__USE_FK
      foreign key (User_id)
-     references USER(User_id);
+     references USER(User_id)
+     on update cascade
+     on delete cascade;
 
 alter table Mi_piace add constraint FKMi__POS
      foreign key (Post_id)
-     references POST(Post_id);
+     references POST(Post_id)
+     on update cascade
+     on delete cascade;
 
 alter table Segue add constraint FKSEGUITO
      foreign key (Seguito_User_id)
-     references USER(User_id);
+     references USER(User_id)
+     on update cascade
+     on delete cascade;
 
 alter table Segue add constraint FKSEGUACE_FK
      foreign key (Seguace_User_id)
-     references USER(User_id);
+     references USER(User_id)
+     on update cascade
+     on delete cascade;
 
 
 -- Index Section

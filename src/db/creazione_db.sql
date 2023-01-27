0b7a1cece2a1124718920a8403449c1c92aeb884
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-01-27 17:16:37 +0100

    modified creazione_db.sql and inserisci_dati.sql

--- src/db/creazione_db.sql
+++ src/db/creazione_db.sql
@@ -11,7 +11,7 @@ use db_progetto_tech_web;
 -- Tables Section
 -- _____________ 
 
-CREATE TABLE db_progetto_tech_web.POST (
+CREATE TABLE db_progetto_tech_web.Post (
      Post_id int not null auto_increment,
      Immagine char(100) not null,
      Testo char(100) not null,
@@ -28,7 +28,7 @@ CREATE TABLE db_progetto_tech_web.Commento (
      Testo char(100) not null,
      constraint ID_Commento_ID primary key (Comment_id));
 
-CREATE TABLE db_progetto_tech_web.TAG (
+CREATE TABLE db_progetto_tech_web.Tag (
      Tag_id int not null auto_increment,
      Nome_gioco char(30) not null,
      constraint ID_TAG_ID primary key (Tag_id));
@@ -43,11 +43,11 @@ CREATE TABLE db_progetto_tech_web.Segue (
      Seguito_User_id int not null,
      constraint ID_Segue_ID primary key (Seguito_User_id, Seguace_User_id));
 
-CREATE TABLE db_progetto_tech_web.USER (
+CREATE TABLE db_progetto_tech_web.User_table (
      User_id int not null auto_increment,
      Username char(30) not null,
      E_mail char(30) not null,
-     Password char(30) not null,
+     Passwrd char(30) not null,
      Immagine_profilo char(100),
      constraint ID_USER_ID primary key (User_id));
 
@@ -55,51 +55,51 @@ CREATE TABLE db_progetto_tech_web.USER (
 -- Constraints Section
 -- ___________________ 
 
-alter table POST add constraint FKTaggato_FK
+alter table Post add constraint FKTaggato_FK
      foreign key (Tag_id)
-     references TAG(Tag_id)
+     references Tag(Tag_id)
      on update cascade
      on delete cascade;
 
-alter table POST add constraint FKPostato_FK
+alter table Post add constraint FKPostato_FK
      foreign key (User_id)
-     references USER(User_id)
+     references User_table(User_id)
      on update cascade
      on delete cascade;
 
 alter table Commento add constraint FKRiferito_FK
      foreign key (Post_id)
-     references POST(Post_id)
+     references Post(Post_id)
      on update cascade
      on delete cascade;
 
 alter table Commento add constraint FKCreato
      foreign key (User_id)
-     references USER(User_id)
+     references User_table(User_id)
      on update cascade
      on delete cascade;
 
 alter table Mi_piace add constraint FKMi__USE_FK
      foreign key (User_id)
-     references USER(User_id)
+     references User_table(User_id)
      on update cascade
      on delete cascade;
 
 alter table Mi_piace add constraint FKMi__POS
      foreign key (Post_id)
-     references POST(Post_id)
+     references Post(Post_id)
      on update cascade
      on delete cascade;
 
 alter table Segue add constraint FKSEGUITO
      foreign key (Seguito_User_id)
-     references USER(User_id)
+     references User_table(User_id)
      on update cascade
      on delete cascade;
 
 alter table Segue add constraint FKSEGUACE_FK
      foreign key (Seguace_User_id)
-     references USER(User_id)
+     references User_table(User_id)
      on update cascade
      on delete cascade;
 
@@ -107,14 +107,14 @@ alter table Segue add constraint FKSEGUACE_FK
 -- Index Section
 -- _____________ 
 
-create unique index ID_POST_IND
-     on POST (Post_id);
+create unique index ID_Post_IND
+     on Post (Post_id);
 
 create index FKTaggato_IND
-     on POST (Tag_id);
+     on Post (Tag_id);
 
 create index FKPostato_IND
-     on POST (User_id);
+     on Post (User_id);
 
 create unique index ID_Commento_IND
      on Commento (User_id, Post_id, Comment_id);
@@ -123,7 +123,7 @@ create index FKRiferito_IND
      on Commento (Post_id);
 
 create unique index ID_TAG_IND
-     on TAG (Tag_id);
+     on Tag (Tag_id);
 
 create unique index ID_Mi_piace_IND
      on Mi_piace (Post_id, User_id);
@@ -138,4 +138,4 @@ create index FKSEGUACE_IND
      on Segue (Seguace_User_id);
 
 create unique index ID_USER_IND
-     on USER (User_id);
+     on User_table (User_id);
--- src/db/inserisci_dati.sql
+++ src/db/inserisci_dati.sql
@@ -1,1 +1,35 @@
--- file vuoto che contterrà il codice sql per popolare il db
+-- file used to populate the database
+
+use db_progetto_tech_web;
+
+-- inserting users into user_table
+
+INSERT INTO User_table(Username, E_mail, Passwrd, Immagine_profilo) Values("leonardo.randacio", "leorand@amail.com", "tonnoConForza", "profile-1.jpg");
+
+INSERT INTO User_table(Username, E_mail, Passwrd, Immagine_profilo)Values("vdamianob", "vdamianb@amail.com", "12345", "profile-2.jpg");
+
+INSERT INTO User_table(Username, E_mail, Passwrd, Immagine_profilo) Values("filusbrius", "filippo@amail.com", "ioSonoFilippo", "profile-3.jpg");
+
+-- inserting tags into Tag table
+
+INSERT INTO Tag(Nome_gioco) VALUES ("Final Fantasy");
+
+INSERT INTO Tag(Nome_gioco) VALUES ("Grand Theft Auto");
+
+-- inserting posts into post table
+
+INSERT INTO Post(Immagine, Testo, Giorno, Ora, Tag_id, User_id) Values("post-example-1.jpg", "guardate che figo mamma mia", "07/01/2022", "14:00", (SELECT Tag_id FROM Tag WHERE Nome_gioco = "Final Fantasy"), (SELECT User_id FROM User_table WHERE Username = "leonardo.randacio"));
+
+INSERT INTO Post(Immagine, Testo, Giorno, Ora, Tag_id, User_id) Values("post-example-2.jpg", "bug incredibile in Grand Theft Auto, da non credere", "08/02/2022", "13:00", (SELECT Tag_id FROM Tag WHERE Nome_gioco = "Grand Theft Auto"), (SELECT User_id FROM User_table WHERE Username = "vdamianob"));
+
+-- inserting comments into commento table
+
+INSERT INTO Commento(User_id, Post_id, Testo) Values((SELECT User_id FROM User_table WHERE Username = "vdamianob"), (SELECT Post_id FROM Post WHERE Immagine = "post-example-1.jpg"), "AHAHA hai proprio ragione");
+
+-- inserting likes into mi_piace table
+
+INSERT INTO Mi_piace(Post_id, User_id) VALUES ((SELECT User_id FROM User_table WHERE Username = "vdamianob"), (SELECT Post_id FROM Post WHERE Immagine = "post-example-1.jpg"));
+
+-- inserting follower into segue table
+
+INSERT INTO Segue(Seguace_User_id, Seguito_User_id) VALUES((SELECT User_id FROM User_table WHERE Username = "vdamianob"), (SELECT User_id FROM User_table WHERE Username = "leonardo.randacio"));

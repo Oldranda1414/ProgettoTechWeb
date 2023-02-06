9594e31a1713f97a28c2c5756c206d0389cb2f18
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-02-06 16:22:57 +0100

    fixed merge problems

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,9 +3,5 @@ session_start();
 define("UPLOAD_DIR", "./upload/");
 require_once("utils/functions.php");
 require_once("db/database.php");
-<<<<<<< HEAD
-$dbh = new DatabaseHelper("81.56.31.11", "utente", "!Progetto12345", "db_progetto_tech_web", 3306);
-=======
 $dbh = new DatabaseHelper("localhost", "root", "", "db_progetto_tech_web", 3306);
->>>>>>> 47aa49d52055741c977515c1ff0c6985a0c6a34e
 ?>

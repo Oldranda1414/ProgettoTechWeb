e17fc2a38d3ea25961df865a34afe3ebd2884973
Vittorio Damiano Brasini - vittorio.brasini@studio.unibo.it <vittorio.brasini@studio.unibo.it>
2023-02-09 18:11:16 +0100

    Added things in menu bar

--- src/template/base.php
+++ src/template/base.php
@@ -14,13 +14,14 @@
 <body class="bg-1">
 	<nav class="navbar navbar-expand-lg bg-2">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="index.php">
-				<img src= "<?php echo UPLOAD_DIR."gamepad_logo.png"?>" alt="Logo" width="38" height="24"
+			<a class="navbar-brand" href="./home.html">
+				<img src="<?php echo UPLOAD_DIR."gamepad_logo.png"?>" alt="Logo" width="38" height="24"
 					class="d-inline-block align-text-top">
 				Life&Games
 			</a>
 			<div class="d-flex flex-row order-2 order-lg-3">
 				<ul class="navbar-nav flex-row px-4">
+					<li class="nav-item"><a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#newPostModal"><img src="./upload/icons/add-post.png" height="30" alt="add post icon"></a></li>
 					<li class="nav-item"><a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#notificationModal"><img src="./upload/icons/bell2.png" height="30" alt="notification bell button"><sup class="notification-number">3</sup></a></li>
 				</ul>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,6 +39,96 @@
 			</div>
 		</div>
 	</nav>
+	
+	<div class="modal fade" id="newPostModal" tabindex="-1" aria-labelledby="newPostModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content bg-3">
+				<div class="modal-header">
+					<img src="./images/profiles/profile-1.jpg" class="modal-post-img-profile me-2" alt="profile icon"
+						height="40">
+					<h1 class="modal-title fs-5" id="postModalLabel">Nuovo post</h1>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<form>
+						<div class="mb-3">
+							<input class="form-control me-2 bg-4  mb-2" type="search" placeholder="Inserisci tag..." aria-label="Search">
+							<label for="message-text" class="col-form-label">Testo:</label>
+							<textarea class="form-control" id="message-text" maxlength="250"></textarea>
+						</div>
+					</form>
+					<hr>
+					<h2 class="fs-5 text-center">
+						Carica immagine
+					</h2>
+					<div class="mb-3">
+						<input class="form-control" type="file" id="formFile" accept=".jpg,.png">
+					</div>
+					
+				</div>
+				<div class="modal-footer">
+					<!-- <button type="button" class="btn btn-primary">Mi piace</button> -->
+					<button type="button" class="btn btn-info">Pubblica post</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="modal fade" id="notificationModal" tabindex="-1" aria-labelledby="notificationModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content bg-5">
+				<div class="modal-header">
+					<img src="./images/icons/bell.png" class="modal-post-img-profile me-2" alt="profile icon" height="40">
+					<h1 class="modal-title fs-5" id="postModalLabel">Notifiche</h1>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<div class="post-comment mt-2 mb-5">
+						<p class="text-muted">
+							<img src="./images/profiles/profile-3.jpg" class="modal-post-img-profile me-2" alt="comment profile icon" height="40">
+							<em class="nickname-label">Xut-polisH</em>, il <em class="modal-comment-post-date">04.08.2023</em> alle <em class="modal-comment-post-time">18:39</em>
+						</p>
+						<p class="text-post-comment">
+							Ha messo <em>Mi piace</em> al <a href="">post</a> che hai pubblicato il <em>18.11.2022</em> alle <em>18:15</em>.
+						</p>
+					</div>
+					<hr>
+					<div class="post-comment mt-2 mb-5">
+						<p class="text-muted">
+							<img src="./images/profiles/profile-3.jpg" class="modal-post-img-profile me-2" alt="comment profile icon" height="40">
+							<em class="nickname-label">Xut-polisH</em>, il <em class="modal-comment-post-date">04.08.2023</em> alle <em class="modal-comment-post-time">18:39</em>
+						</p>
+						<p class="text-post-comment">
+							Ha messo <em>Mi piace</em> al <a href="">post</a> che hai pubblicato il <em>18.11.2022</em> alle <em>18:15</em>.
+						</p>
+					</div>
+					<hr>
+					<div class="post-comment mt-2 mb-5">
+						<p class="text-muted">
+							<img src="./images/profiles/profile-2.jpg" class="modal-post-img-profile me-2" alt="comment profile icon" height="40">
+							<em class="nickname-label">Can$as</em>, il <em class="modal-comment-post-date">04.08.2023</em> alle <em class="modal-comment-post-time">18:39</em>
+						</p>
+						<p class="text-post-comment">
+							Ha commentato il <a href="">post</a> che hai pubblicato il <em>18.11.2022</em> alle <em>18:15</em>.
+						</p>
+					</div>
+					<hr>
+					<div class="post-comment mt-2 mb-5">
+						<p class="text-muted">
+							<img src="./images/profiles/profile-2.jpg" class="modal-post-img-profile me-2" alt="comment profile icon" height="40">
+							<em class="nickname-label">Can$as</em>, il <em class="modal-comment-post-date">04.08.2023</em> alle <em class="modal-comment-post-time">18:39</em>
+						</p>
+						<p class="text-post-comment">
+							Ha cominciato a seguirti.
+						</p>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-info">Cancella tutto</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<main>
 		<?php
@@ -59,3 +150,5 @@
 </body>
 
 </html>
+
+

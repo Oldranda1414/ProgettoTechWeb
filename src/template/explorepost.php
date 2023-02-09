90f1660a2d81130a8ecce7759be75b70849aaefc
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-02-09 18:10:34 +0100

    added post visualization to explore post

--- src/template/explorepost.php
+++ src/template/explorepost.php
@@ -40,7 +40,7 @@
 								<?php echo $elemento["Words"]?>
 							</p>
 							<div class="like-number text-end me-3">
-								<?php echo $elemento["NumberOfLikes"]?>
+								numero di mi piace: <?php echo $elemento["NumberOfLikes"]?>
 							</div>
 							<hr>
 							<h2 class="fs-5 text-center">

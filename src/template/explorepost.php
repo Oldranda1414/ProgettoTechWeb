4cd5bda9b24d82ef60fa96f676d822382e6a72ba
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-02-10 12:44:32 +0100

    solved modal form for carousel

--- src/template/explorepost.php
+++ src/template/explorepost.php
@@ -32,7 +32,7 @@
 
 						<div class="carousel-caption d-md-block">
 							<div class="title-carousel-post text-truncate" data-bs-toggle="modal"
-								data-bs-target="#postModal"><?php echo $elemento["Tag"]?></div>
+								data-bs-target="#post<?php echo $elemento["Post_id"] ?>ModalCarousel"><?php echo $elemento["Tag"]?></div>
 							<div class="row justify-content-center">
 								<div class="col">
 									<img src="<?php echo UPLOAD_DIR."profiles/".$elemento["UserProfilePic"]; ?>" class="rounded-circle shadow-lg mr-3"
@@ -44,7 +44,7 @@
 								<?php echo $elemento["Words"]?>
 							</p>
 							<div class="open-post d-none d-sm-block d-md-block" data-bs-toggle="modal"
-								data-bs-target="#postModal">Apri post</div>
+								data-bs-target="#post<?php echo $elemento["Post_id"] ?>ModalCarousel">Apri post</div>
 						</div>
 					</div>
 					<?php endforeach ?>
@@ -64,6 +64,62 @@
 			</div>
 		</div>
 	</div>
+	<?php foreach ($templateParams["mostLikedPosts"] as $elemento) : ?>
+	<div class="modal fade" id="post<?php echo $elemento["Post_id"] ?>ModalCarousel" tabindex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content bg-3">
+						<div class="modal-header">
+							<img src="<?php echo UPLOAD_DIR."profiles/".$elemento["UserProfilePic"]; ?>" class="modal-post-img-profile me-2" alt="profile icon"
+								height="40">
+							<h1 class="modal-title fs-5" id="postModalLabel"><?php echo $elemento["Tag"]?></h1>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							<p class="text-muted">di <em class="nickname-label"><?php echo $elemento["Username"] ?></em>, il <em
+									class="modal-post-date"><?php echo $elemento["Day_posted"]?></em> alle <em class="modal-post-time"><?php echo $elemento["Time_posted"]?></em></p>
+							<img src="<?php echo UPLOAD_DIR."posts/".$elemento["Img"] ?>" class="card-img-top rounded" alt="Post Image">
+							<p class="post-body my-3">
+								<?php echo $elemento["Words"]?>
+							</p>
+							<div class="like-number text-end me-3">
+								<?php echo $elemento["NumberOfLikes"]?> Mi piace
+							</div>
+							<hr>
+							<h2 class="fs-5 text-center">
+								Commenti
+							</h2>
+							<div class="post-comment-section">
+								<?php foreach($elemento["Comments"] as $singleComment): ?>
+								<div class="post-comment mt-2 mb-5">
+									<p class="text-muted">
+										<img src="<?php echo UPLOAD_DIR."profiles/".$singleComment["Profile_img"] ?>" class="modal-post-img-profile me-2"
+											alt="comment profile icon" height="40">
+										di <em class="nickname-label"><?php echo $singleComment["Username"] ?></em>, il <em
+											class="modal-comment-post-date"><?php echo $singleComment["Day_posted"] ?></em> alle <em
+											class="modal-comment-post-time"><?php echo $singleComment["Time_posted"] ?></em>
+									</p>
+									<p class="text-post-comment">
+										<?php echo $singleComment["Words"] ?>
+									</p>
+								</div>
+								<?php endforeach; ?>
+							</div>
+							<form>
+								<div class="mb-3">
+									<label for="message-text" class="col-form-label">Commenta:</label>
+									<textarea class="form-control" id="message-text"></textarea>
+								</div>
+							</form>
+						</div>
+						<div class="modal-footer">
+							<!-- <button type="button" class="btn btn-primary">Mi piace</button> -->
+							<a href="#" class="btn like-button m-1">Mi piace</a>
+							<button type="button" class="btn btn-info">Invia commento</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endforeach ?>
 <?php endif ?>
 		
 	<div class="container mt-4">

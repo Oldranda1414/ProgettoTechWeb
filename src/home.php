ec23f4c00a28a936b1fbde93ee8d80a3930c3d99
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-05-13 11:36:43 +0200

    added new home.php file to better differentiate between home.php and index.php

---
+++ src/home.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'bootstrap.php';
+
+if(isUserLoggedIn($dbh)){
+    $templateParams["titolo"] = "Home";
+    $templateParams["nome"] = "home.php";
+    $templateParams["posts"] = $dbh->getFullPosts(8);
+    require 'template/base.php';
+}
+else{
+    require 'login.php';
+}
+
+?>

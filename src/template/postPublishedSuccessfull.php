cf5a9638d40e6837be466c616f0fa146c5b2317f
vdamianob <vittorio.brasini@studio.unibo.it>
2023-06-02 00:19:45 +0200

    adjusted margin of toast message (post published usccessfull)

--- src/template/postPublishedSuccessfull.php
+++ src/template/postPublishedSuccessfull.php
@@ -1,5 +1,5 @@
 	  	<!-- qua comincia la notifica toast "POST PUBBLICATO CON SUCCESSO" -->
-          <div aria-live="" aria-atomic="true" class="" style="position: absolute; min-height: 200px; z-index:5">
+          <div aria-live="" aria-atomic="true" class="m-3" style="position: absolute; min-height: 200px; z-index:5">
 	  <div class="toast bg-4" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3300">
 		<div class="toast-header bg-3">
 		  <img src="<?php echo UPLOAD_DIR."icons/success.png" ?>" class="rounded mr-2 alert-icon" alt="post published icon">

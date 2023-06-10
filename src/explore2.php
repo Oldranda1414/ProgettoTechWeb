981e089245f23674f6aab2eddfad578956faae9b
Filippo Velli <filippo.velli@studio.unibo.it>
2023-06-10 19:17:11 +0200

    Updated the function to the new one using ['user_id']

--- src/explore2.php
+++ src/explore2.php
@@ -6,7 +6,7 @@ if(isUserLoggedIn($dbh)){
     $templateParams["titolo"] = "Esplora";
     $templateParams["nome"] = "explore2.php";
     //TODO update date with todays date using {date("y-m-d")}
-    $templateParams["mostLikedPosts"] = $dbh->getMostLikedPosts("2022-01-07", 3);
+    $templateParams["mostLikedPosts"] = $dbh->getMostLikedPosts($_SESSION['user_id'], "2022-01-07", 3);
     array_push($templateParams["js"], "js/explore.js", "js/posts-home.js", "https://unpkg.com/axios/dist/axios.min.js");
     require 'template/base.php';
 }

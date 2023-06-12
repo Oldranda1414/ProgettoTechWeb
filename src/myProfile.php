e5171696c567399230fe979edd2842c1ad9d0443
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-06-12 17:09:39 +0200

    restored some lines deleted in transition to posts.js

--- src/myProfile.php
+++ src/myProfile.php
@@ -2,38 +2,51 @@
 require_once 'bootstrap.php';
 require "base.php";
 
-if(isUserLoggedIn($dbh)){
+if (isUserLoggedIn($dbh)) {
+
+    //update password api
+    if (isset($_POST["newp"]) || isset($_POST["p"])) {
+        if (!empty($_POST["newp"]) || !empty($_POST["p"])) {
+            list($result, $errorMsg) = $dbh->secureLoginUser($_SESSION["username"], $_POST["p"]);
+            if ($result != 0) {
+                $dbh->updatePassword($_SESSION['user_id'], $_POST["newp"]);
+                var_dump("password aggiornata");
+            } else {
+                //TODO PASSWORD NON CORRETTA NOTIFICARE UTENTE (errore contenuto in $errorMsg)
+            }
+        } else {
+            //TODO NOTIFICARE UTENTE. DEI CAMPI SONO VUOTI
+        }
+    }
+
+
     //update profile img api
-    if(isset($_FILES["updateProfileImg"])){
-        if(strlen($_FILES["updateProfileImg"]["name"])>0){
+    if (isset($_FILES["updateProfileImg"])) {
+        if (strlen($_FILES["updateProfileImg"]["name"]) > 0) {
             //deleting old img from server
-            deleteImg(UPLOAD_DIR."profiles/".$templateParams["user"]["Profile_img"]);
+            deleteImg(UPLOAD_DIR . "profiles/" . $templateParams["user"]["Profile_img"]);
             //uploading new profile img
-            list($imgUploadResult, $imgUploadMsg) = uploadImage(UPLOAD_DIR."profiles/", $_FILES["updateProfileImg"]);
-            if($imgUploadResult != 0){
+            list($imgUploadResult, $imgUploadMsg) = uploadImage(UPLOAD_DIR . "profiles/", $_FILES["updateProfileImg"]);
+            if ($imgUploadResult != 0) {
                 //updating db
                 $dbh->updateProfileImg($_SESSION["user_id"], $imgUploadMsg);
                 //updating $templateParams["user"]
                 $templateParams["user"]["Profile_img"] = $_FILES["updateProfileImg"]["name"];
-            }
-            else{
+            } else {
                 //TODO ERRORE CARICAMENTO IMMAGINE, NOTIFICARE UTENTE (il tipo di errore è contenuto in $imgUploadMsg)
             }
-        }
-        else{
+        } else {
             //TODO NOTIFICARE CHE IL FILE CARICATO è VUOTO/errato
         }
     }
     $templateParams["titolo"] = "Mio profilo";
-    $templateParams["nome"] = "myProfile.php"; 
+    $templateParams["nome"] = "myProfile.php";
     $templateParams["likes"] = $dbh->getUserLikes($_SESSION['username']);
     $templateParams["comments"] = $dbh->getUserComments($_SESSION['username']);
     $templateParams["followers"] = $dbh->getUserFollowers($_SESSION['username']);
     $templateParams["followed"] = $dbh->getUserFollowed($_SESSION['username']);
-    array_push ($templateParams["js"],"https://unpkg.com/axios/dist/axios.min.js", "js/changePassword.js", "js/posts.js");
+    array_push($templateParams["js"], "https://unpkg.com/axios/dist/axios.min.js", "js/changePassword.js", "js/posts.js", "js/myProfile.js", "js/sha512.js");
     require 'template/base.php';
-}
-else{
+} else {
     require 'login.php';
 }
-?>

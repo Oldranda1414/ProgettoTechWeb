81e375ead71b9da1c6683b351909f070e1db481e
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-06-15 11:39:12 +0200

    fixed minor bug

--- src/template/notificationComment.php
+++ src/template/notificationComment.php
@@ -2,7 +2,7 @@
                         	<a href="profile.php?Username=<?php echo $notification["Commenter_Username"] ?>">
                         		<p class="text-muted">
                         			<img src="<?php echo UPLOAD_DIR . "profiles/" . $notification["Commenter_Profile_img"] ?>" class="modal-post-img-profile me-2" alt="like profile icon" height="40">
-                        			<em class="nickname-label"><?php echo $notification["Commenter_Username"] ?></em>, il <em class="modal-comment-post-date"><?php echo date("d-m-y", strtotime($notification["DT"])) ?></em> alle <em class="modal-comment-post-time"><?php date("H:i:s", strtotime($notification["DT"])) ?></em>
+                        			<em class="nickname-label"><?php echo $notification["Commenter_Username"] ?></em>, il <em class="modal-comment-post-date"><?php echo date("d-m-y", strtotime($notification["DT"])) ?></em> alle <em class="modal-comment-post-time"><?php echo date("H:i:s", strtotime($notification["DT"])) ?></em>
                         		</p>
                         	</a>
                         	<p class="text-post-comment">

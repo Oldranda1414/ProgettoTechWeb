6143ca8f9fae91d1b30d34e6264a3a4a5310ba5c
Vittorio Damiano Brasini <vittorio.brasini@studio.unibo.it>
2023-06-15 02:11:44 +0200

    changed colors

--- src/template/successToast.php
+++ src/template/successToast.php
@@ -1,7 +1,7 @@
 <div aria-live="" aria-atomic="true" class="m-3" style="position: absolute; min-height: 200px; z-index:5">
     <div class="toast bg-4" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3300">
         <div class="toast-header bg-3">
-            <img src="<?php echo UPLOAD_DIR . "icons/success.png" ?>" class="rounded mr-2 alert-icon" alt="post published icon">
+            <img src="<?php echo UPLOAD_DIR . "icons/success.png" ?>" class="rounded mr-2 alert-icon" alt="post published icon" style="width:25px">
             <strong class="mr-auto">Avviso</strong>
         </div>
         <div class="toast-body">
